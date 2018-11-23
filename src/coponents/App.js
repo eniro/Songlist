f439e8f2683349058a63468b67c61abda599f458
Iro Enezli <enezliro@gmail.com>
2018-11-23 14:38:40 +0200

    First commit

---
+++ src/coponents/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+      <div>App
+      </div>
+    );
+};
+
+export default App;
