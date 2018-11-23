a8a91734d8d10b5399b33585863f88021a74004f
Iro Enezli <enezliro@gmail.com>
2018-11-23 15:19:18 +0200

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
     return [ 
         { title: 'Hign Hopes', duration: '05:16'},
@@ -12,4 +14,11 @@ const selectedSongReducer = (selectedSong=null, action) => {
     if( action.type === 'SONG_SELECTED') {
         return action.payload;
     }
-}
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
